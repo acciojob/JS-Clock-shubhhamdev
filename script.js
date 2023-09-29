361e8912a92119c9b384add659792b329e8f69e6
Shubham Dev <133051223+shubhhamdev@users.noreply.github.com>
2023-09-29 19:07:16 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,37 +1,4 @@
- // const hourHand = document.querySelector('.hour-hand');
- //    const minuteHand = document.querySelector('.min-hand');
- //    const secondHand = document.querySelector('.second-hand');
-
- //    // Set the initial rotation of the hands
- //    hourHand.style.transform = 'rotate(0deg)';
- //    minuteHand.style.transform = 'rotate(0deg)';
- //    secondHand.style.transform = 'rotate(0deg)';
-
- //    function updateClock() {
- //      const now = new Date();
-
- //      const hours = now.getHours();
- //      const minutes = now.getMinutes();
- //      const seconds = now.getSeconds();
-
- //      // Calculate the rotation of the hour hand
- //      const hourRotation = (hours % 12) * 360 / 12 + (minutes / 60) * 360 / 12 + 90;
-
- //      // Calculate the rotation of the minute hand
- //      const minuteRotation = (minutes * 360 / 60) + (seconds / 60) * 360 / 60 + 90;
-
- //      // Calculate the rotation of the second hand
- //      const secondRotation = (seconds * 360 / 60) + 90;
-
- //      // Set the rotation of the hands
- //      hourHand.style.transform = `rotate(${hourRotation}deg)`;
- //      minuteHand.style.transform = `rotate(${minuteRotation}deg)`;
- //      secondHand.style.transform = `rotate(${secondRotation}deg)`;
- //    }
-
- //    // Update the clock every second
- //    setInterval(updateClock, 1000);
-    const hourHand = document.querySelector('.hour-hand');
+ const hourHand = document.querySelector('.hour-hand');
     const minuteHand = document.querySelector('.min-hand');
     const secondHand = document.querySelector('.second-hand');
 
@@ -39,10 +6,6 @@
     hourHand.style.transform = 'rotate(0deg)';
     minuteHand.style.transform = 'rotate(0deg)';
     secondHand.style.transform = 'rotate(0deg)';
-
-    function degreesToRadians(degrees) {
-      return (degrees * Math.PI) / 180;
-    }
 
     function updateClock() {
       const now = new Date();
